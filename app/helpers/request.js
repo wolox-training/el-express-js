fdb576dfc4b12534195bd0ce10af10b2d5f4aa25
Edilson Londoño <edilson.londono@accenture.com>
2022-01-05 16:07:47 -0500

    The error was modified by defaultError in service numbers  and the request file in the folder helper was deleted

--- app/helpers/request.js
+++
@@ -1,6 +0,0 @@
-const axios = require('axios');
-
-exports.get = async (url, params) => {
-  const data = await axios.get(url, params);
-  return data;
-};
--- app/services/numbers.js
+++ app/services/numbers.js
@@ -1,17 +1,18 @@
-const request = require('../helpers/request');
+const axios = require('axios');
 const {
   common: { numbersApi }
 } = require('../../config');
-const { SERVER_ERROR } = require('../constants/errors');
+const errors = require('../errors');
+const { DEFAULT_ERROR } = require('../constants/errors');
 const logger = require('../logger');
 
 exports.getSentence = async () => {
   const { url } = numbersApi;
   try {
-    const data = await request.get(url);
+    const data = await axios.get(url);
     return data.data;
   } catch (err) {
     logger.error(err);
-    throw Error(SERVER_ERROR);
+    throw errors.defaultError(DEFAULT_ERROR);
   }
 };
