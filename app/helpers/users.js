777abd02ae809658256ad167e59b4dcad6ca492b
Edilson Londoño <edilson.londono@accenture.com>
2022-01-14 14:26:07 -0500

    Constants included

--- app/helpers/users.js
+++ app/helpers/users.js
@@ -1,8 +1,17 @@
+const {
+  DEVELOPER_POSITION,
+  LEAD_POSITION,
+  TL_POSITION,
+  EM_POSITION,
+  HEAD_POSITION,
+  CEO_POSITION
+} = require('../constants/users');
+
 exports.getPosition = score => {
-  if (score < 5) return 'Developer';
-  if (score <= 9) return 'Lead';
-  if (score <= 19) return 'TL';
-  if (score <= 29) return 'EM';
-  if (score <= 40) return 'HEAD';
-  return 'CEO';
+  if (score < 5) return DEVELOPER_POSITION;
+  if (score <= 9) return LEAD_POSITION;
+  if (score <= 19) return TL_POSITION;
+  if (score <= 29) return EM_POSITION;
+  if (score <= 49) return HEAD_POSITION;
+  return CEO_POSITION;
 };
